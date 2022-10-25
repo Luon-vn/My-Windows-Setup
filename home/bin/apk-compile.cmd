af7efb3566bac5edcd735ea37ca89dad79f8cd64
vinhjaxt <ngoc.lo.dang@gmail.com>
2022-10-26 01:25:59 +0700

    Add files via upload

--- home/bin/apk-compile.cmd
+++ home/bin/apk-compile.cmd
@@ -4,12 +4,26 @@ setlocal enableDelayedExpansion
 set "output="
 set "dirPath=%1"
 
-for /F "delims=" %%J in ("%dirPath%") do for /F "delims=" %%I in ("%%~fJ.") do set "output=%%~fI"
+if exist "%dirPath%\*" (
+  for /F "delims=" %%J in ("%dirPath%") do for /F "delims=" %%I in ("%%~fJ.") do set "output=%%~fI"
 
-if "!output:~-1!"=="\" set "output=app"
+  if "!output:~-1!"=="\" set "output=app"
 
-echo Compiling into !output!-unsigned.apk..
-call "%~dp0\apktool.cmd" b -f -r -o "!output!-unsigned.apk" %*
+  echo Compiling into !output!-unsigned.apk..
+  call "%~dp0\apktool.cmd" b -f -r -o "!output!-unsigned.apk" %*
+) else (
+  rem If file
+  for /F "delims=" %%i in ("%1") do (
+    set "basename=%%~ni"
+    set "parentpath=%%~dpi"
+  )
+
+  set "output=!parentpath!!basename!-resign"
+
+  del /S /Q "!output!-unsigned.apk" >.\NUL 2>&1
+  copy "!dirPath!" "!output!-unsigned.apk"
+  zip -d "!output!-unsigned.apk" "META-INF/*"
+)
 
 @REM Generate keystore
 set KEYSTORE_FILE=D:\home\my-release-key.keystore
@@ -17,15 +31,15 @@ set KEYSTORE_ALIAS=my-key-alias
 set KEYSTORE_PASSWORD=12345678
 @REM keytool -genkey -v -keystore "%KEYSTORE_FILE%" -alias "%KEYSTORE_ALIAS%" -keyalg RSA -keysize 2048 -validity 10000 -storetype pkcs12
 
-echo Signing !output!-unsigned.apk..
-@REM jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore your-release-key.keystore android-release-unsigned.apk alias -storepass password
-jarsigner -keystore "%KEYSTORE_FILE%" -storepass "%KEYSTORE_PASSWORD%" "!output!-unsigned.apk" "%KEYSTORE_ALIAS%"
-
 del /S /Q "!output!.apk" >.\NUL 2>&1
 echo Zipaligning !output!-unsigned.apk..
 @REM sdkmanager "platforms;android-30" "build-tools;30.0.0" --sdk_root=D:\home\android-sdk\
 zipalign 4 "!output!-unsigned.apk" "!output!.apk"
 
-@REM del /S /Q "!output!-unsigned.apk" >.\NUL 2>&1
+echo Signing !output!.apk..
+@REM jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore your-release-key.keystore android-release-unsigned.apk alias -storepass password
+jarsigner -keystore "%KEYSTORE_FILE%" -storepass "%KEYSTORE_PASSWORD%" "!output!.apk" "%KEYSTORE_ALIAS%"
+
+del /S /Q "!output!-unsigned.apk" >.\NUL 2>&1
 echo Run:
 echo adb install !output!.apk
